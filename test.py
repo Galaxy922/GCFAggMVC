6862dcf256990fd6192177b1c6838740533df56f
Galaxy922 <118326427+Galaxy922@users.noreply.github.com>
2023-06-16 19:15:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ from dataloader import load_data
 # Caltech-3V
 # Caltech-4V
 # Caltech-5V
-Dataname = 'Synthetic3d'
+Dataname = 'Hdigit'
 parser = argparse.ArgumentParser(description='test')
 parser.add_argument('--dataset', default=Dataname)
 parser.add_argument('--batch_size', default=256, type=int)
